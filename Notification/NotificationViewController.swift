33096882c4ebcbb76c77b0e974fe78193d1a75ae
Akos Birmacher <akos.birmacher@bitrise.io>
2019-11-11 16:33:30 +0100

    add notification content extension target

---
+++ Notification/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  Notification
+//
+//  Created by Akos Birmacher on 2019. 11. 11..
+//  Copyright © 2019. Bitrise. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
